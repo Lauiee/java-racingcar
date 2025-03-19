748b70c690e2556dfe5b7a43393ed48e60ea4f4d
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-03-19 19:22:27 +0900

    feat: Car클래스 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+public class Car {
+
+    private int position;
+
+    public Car() {
+        this.position = 0;
+    }
+
+    public void move(){
+        this.position++;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+}
