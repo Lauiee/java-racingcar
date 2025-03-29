93ba6aef66854ce124665a2e33f28f55fac97389
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-03-29 15:37:25 +0900

    refactor: 변경된 클래스들에 맞게 수정

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,53 +1,40 @@
 package racingcar.controller;
 
 import java.util.List;
-import racingcar.factory.CarsFactory;
 import racingcar.model.Car;
 import racingcar.model.Cars;
-import racingcar.service.RacingService;
-import racingcar.util.CarNameParser;
-import racingcar.util.TimesParser;
-import racingcar.validator.CarNamesValidation;
-import racingcar.validator.TimesValidation;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingController {
 
-    private final RacingService racingService;
     private final OutputView outputView;
     private final InputView inputView;
 
-    public RacingController(RacingService racingService, OutputView outputView, InputView inputView) {
-        this.racingService = racingService;
+    public RacingController(OutputView outputView, InputView inputView) {
         this.outputView = outputView;
         this.inputView = inputView;
-
     }
 
     public void run(){
 
         // 경주 준비
-        String input = inputView.carsInput();
-        CarNamesValidation.validateAllInput(input);
-        List<String> carNames = CarNameParser.parseCarName(input);
-        carNames.forEach(CarNamesValidation::validateCarName);
-        Cars cars = CarsFactory.createCars(carNames);
+        List<String> carNames = inputView.carsInput();
+        int times = inputView.timesInput();
 
-        String timeInput = inputView.timesInput();
-        TimesValidation.validateAllInput(timeInput);
-        int times = TimesParser.parseTimes(timeInput);
+        Cars cars = Cars.from(carNames);
 
         // 경주 시작
         outputView.printResultMessage();
         for (int i = 0; i<times; i++){
-            racingService.doRacing(cars);
+            cars.doRacing();
             outputView.printResult(cars);
         }
 
         // 경주 결과
-        List<Car> winner = racingService.getWinner(cars);
-        outputView.printWinner(winner);
+        List<Car> winners = cars.getWinners();
+        outputView.printWinner(winners);
     }
+
 
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -20,21 +20,5 @@ class CarTest {
         car.move();
         assertThat(car.getName()).isEqualTo("test : -");
     }
-
-    @Test
-    @DisplayName("현재 선두의 위치와 대상 자동차의 위치를 비교해 더 앞선 위치로 업데이트 합니다.")
-    void 선두_위치_업데이트(){
-        int currentTopPosition = 0;
-        car.move();
-        int updatedPosition = car.updateTopPosition(currentTopPosition);
-        assertThat(updatedPosition).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("최종 선두 위치와 대상 자동차의 위치를 비교해 우승자 여부를 판단합니다.")
-    void 우승자_여부_판단(){
-        int topPosition = 0;
-        assertThat(car.isWinner(topPosition)).isTrue();
-    }
     
 }
